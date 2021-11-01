d0e78a2923bf89da18cb91a24882232e73bfc8cd
Tapiwa Rutsito <trutsito@delta.co.zw>
2021-11-01 16:45:03 +0200

    merging all branches

---
+++ src/components/modals/AllocateModal.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100%;
+`;
+
+export function AllocateModal(props) {
+  return <Container></Container>;
+}
